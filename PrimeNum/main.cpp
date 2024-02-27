f8b649e9c2a99ad84e21086578c8289c7f670112
Rossini <rossini@mail.com>
2024-02-26 22:39:43 -0300

    R: implementing class PrimeNumberTable

--- PrimeNum/main.cpp
+++ PrimeNum/main.cpp
@@ -31,7 +31,7 @@ public:
       /* por isso declarei int limit = 0 inicialmente
          enquanto o arrombado nao digitar um numero nao dá sequencia */
       std::cout << "Digite um numero maior que 0" << std::endl;
-      return inputLimit();
+      return inputLimit();  //R: Cara, isso tem cara de chamada recursiva, cuidado ai hem!!!
     }
   }
 };
@@ -41,8 +41,69 @@ public:
 classe principal responsável por manusear o armazenamento
 e a leitura dos números primos gerados <vector>
 R
+R: Como ainda não sei usar a classe <vetor>, 
+vou implementar como tenho costume de fazer depois melhoramos isso
+R: Sempre que se le se escreve na tabela o indice é incrementado automaticamete para próxima posição
 */
-class PrimeNumberTable {};
+class PrimeNumberTable {
+
+public:
+  void init();
+  unsigned long readTable();
+  unsigned long readTable(unsigned long index);  //sobrecarga para admitir escolha aleatória do índice
+  bool writeTable (unsigned long primeNumber);   //responde se foi possível escrever na tabela
+
+private:
+  void clearTable();
+  void indexTableInit();  
+  unsigned long table[];            //R: por via de dúvidas, usemos números grandes
+  unsigned long indexReadTable;     //R: por via de dúvidas, usemos tabelas grandes
+  unsigned long indexWriteTable;  
+
+};
+
+void PrimeNumberTable::init() {
+
+  clearTable();
+  indexTableInit();
+
+}
+
+unsigned long PrimeNumberTable::readTable() {
+
+  if (indexReadTable >= sizeof(table)) return 0;
+  return table[indexReadTable++];   //R: usa indexReadTable, depois incrementa
+
+}
+
+unsigned long PrimeNumberTable::readTable(unsigned long index) {
+
+  indexReadTable = index;
+  readTable();
+
+}
+
+bool PrimeNumberTable::writeTable (unsigned long primeNumber) {
+
+  if (indexWriteTable >= sizeof(table)) return false;
+  table[indexWriteTable] = primeNumber;
+  indexWriteTable++;
+  return true;   
+
+}
+
+void  PrimeNumberTable::clearTable() {
+
+  unsigned long index;
+  for (index = 0; index < sizeof(table); index++) table[index] = NULL; //Limpa dado na posição atual da tabela 
+
+}
+
+void  PrimeNumberTable::indexTableInit() {
+
+  indexReadTable = indexWriteTable = 0;
+
+}
 
 /*
 ===========================================================================
