1a7c6569c7dcc0acbbc963b1ab55be2e04cbe6ff
Mick Maciel <macielmick@gmail.com>
2024-02-28 16:08:47 +0000

    commit ateracao classes

--- PrimeNum/UserInput.cpp
+++ PrimeNum/UserInput.cpp
@@ -8,20 +8,18 @@
 #include "UserInput.hpp"
 #include <iostream>
 
-class UserInput {
-public:
-  int inputLimit() const {
-    int limit = 0;
-    std::cin >> limit;
-    if (limit != 0) {
-      return limit;
-    }
+class UserInput(){public : int inputLimit() const {int limit = 0;
+std::cin >> limit;
+if (limit != 0) {
+  return limit;
+}
 
-    else {
-      /* por isso declarei int limit = 0 inicialmente
-         enquanto o arrombado nao digitar um numero nao dá sequencia */
-      std::cout << "Digite um numero maior que 0" << std::endl;
-      return inputLimit();
-    }
-  }
-};
+else {
+  /* por isso declarei int limit = 0 inicialmente
+     enquanto o arrombado nao digitar um numero nao dá sequencia */
+  std::cout << "Digite um numero maior que 0" << std::endl;
+  return inputLimit();
+}
+}
+}
+;
