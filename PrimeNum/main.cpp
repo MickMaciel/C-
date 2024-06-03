dfe0524db99892b1ae45b5981ce7a634cd34dff2
Mick Maciel <macielmick@gmail.com>
2024-06-03 14:15:52 +0100

    Update main.cpp

--- PrimeNum/main.cpp
+++ PrimeNum/main.cpp
@@ -6,46 +6,25 @@
 
 /*
 ===========================================================================
-M
 */
 int main() {
-		
-//	std::cout << "break point 1          "; system("PAUSE");	
-	
 	// Instanciando objetos das classes
 	PrimeNumbersGenerator primeGenerator;
   UserInput userInput;
   PrimeNumbersTable primeTable(1000);
   
-//	std::cout << "break point 2          "; system("PAUSE");
-reiniciar: // teste
+
+reiniciar:
   //pegar o limit
   int limit = userInput.inputLimit();
-  //unsigned long limit = 30; // teste
-  
-	//std::cout << "break point 3          "; system("PAUSE");
-
-  // Inicializando a tabela de números primos
-  
-  primeGenerator.generate(&primeTable, limit);
-  
- /* 
- 		primeTable.init();
  
- 		for (int number = 0; number < limit; number++) {
-    if (primeGenerator.isPrime(number)) {
-      primeTable.writeTable(number);
-    }
-    
-	std::cout << "break point 4          "; system("PAUSE"); 	    
-  }*/
-  
- //	std::cout << "break point 5          "; system("PAUSE");
+  // Inicializando a tabela de números primos
+  primeGenerator.generate(&primeTable, limit);
 
   // Exibindo os números primos armazenados na tabela
   std::cout << "Numeros primos ate " << limit << ":" << std::endl;
   
-	unsigned long prime;
+unsigned long prime;
   primeTable.initRead();
   while ((prime = primeTable.readTable()) != 0) {
     std::cout << prime << " ";
@@ -53,7 +32,7 @@ reiniciar: // teste
 
  	std::cout << std::endl << std::endl << "break point 6     "; system("PAUSE");
  	
- 	goto reiniciar; //teste
+ 	goto reiniciar; 
  	
   return 0;
 }
